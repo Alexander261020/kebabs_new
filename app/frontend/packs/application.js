9f7dc0571936f013f3f15de3408f2abf7e8123d7
alexandr <89227049120@mail.ru>
2021-12-03 15:13:30 +0500

    first working version. Started server and server webpack

---
+++ app/frontend/packs/application.js
@@ -0,0 +1,7 @@
+import Rails from "@rails/ujs"
+import "channels"
+Rails.start()
+
+import "../styles/application.scss"
+const images = require.context('../images', true)
+// import "bootstrap/dist/js/bootstrap.js"
