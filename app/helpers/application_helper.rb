d1da915677f31e44fd775ed338021d0d488847fc
alexandr <89227049120@mail.ru>
2021-12-03 16:33:18 +0500

    before errors device

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def user_avatar(user)
+    asset_path('user.jpg')
+  end  
+
+  def devise_flash
+    if controller.devise_controller? && resource.errors.any?
+      flash.now[:error] = flash[:error].to_a.concat resource.errors.full_messages
+      flash.now[:error].uniq!
+    end
+  end
 end
