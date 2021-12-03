2aa23fb8280072c3e9d8293d4b38f6fd3158c84a
alexandr <89227049120@mail.ru>
2021-12-03 16:33:18 +0500

    before setup device

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  # Встроенный в девайз фильтр — посылает незалогиненного пользователя
+  before_action :authenticate_user!, except: [:show]
+
+  # Задаем объект @user для шаблонов и экшенов
+  before_action :set_current_user, except: [:show]
+
+  # GET /users/1
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_current_user
+    @user = current_user
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
