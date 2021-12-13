6f3e31c126cfa73e368adbf5705f38c035bb16cb
alexandr <89227049120@mail.ru>
2021-12-13 23:27:47 +0500

    fixed subscription validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,19 +4,23 @@ class Subscription < ActiveRecord::Base
 
   # Обязательно должно быть событие
   validates :event, presence: true
-  validate :event_to_user?
 
-  # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  # Проверки user_name и user_email выполняются,
-  # только если user не задан
-  # То есть для анонимных пользователей
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+ # если юзер на задан
+  with_options if: -> { user.present? } do
+    # Для конкретного event_id один юзер может подписаться только один раз
+    validates :user, uniqueness: { scope: :event_id }
+    validate :event_owner?
+  end
 
-  # Или один email может использоваться только один раз (если анонимная подписка)
-  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
-  validate :email_exist_to_event?, unless: -> { user.present? }
+  # если юзер на задан
+  with_options unless: -> { user.present? } do
+    validates :user_name, presence: true
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+    # Или один email может использоваться только один раз (если анонимная подписка)
+    validates :user_email, uniqueness: { scope: :event_id }
+    validate :email_exist_to_event?
+  end
+
 
   # validates :user_is_not_creator_of_event
   # Если есть юзер, выдаем его имя,
@@ -45,7 +49,7 @@ class Subscription < ActiveRecord::Base
     errors.add(:email, I18n.t('controllers.subscriptions.dubl_email')) if User.exists?(email: user_email)
   end
 
-  def event_to_user?
-    event.user != user || errors.add(:event, I18n.t('controllers.subscriptions.self_subscription'))
+  def event_owner?
+    event.user != user || errors.add(:event, I18n.t('controllers.subscriptions.restrict_self_subscription'))
   end
 end
