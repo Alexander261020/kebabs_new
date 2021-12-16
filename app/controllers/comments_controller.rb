fbceeb7093ab43aa456802d5eb47dd203a8c4c8c
alexandr <myltiplaz@gmail.com>
2021-12-16 14:20:32 +0500

    delete autor from send email to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -52,7 +52,8 @@ class CommentsController < ApplicationController
   def notify_subscribers(event, comment)
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-  
+    # Удаляем автора из рассылки
+    all_emails.delete(comment.user.email)
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
