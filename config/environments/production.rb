e03196d59ba14342e648df8e64cfed19f9901f03
alexandr <myltiplaz@gmail.com>
2021-12-21 12:26:40 +0500

    ettemp next

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,11 +93,35 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # Устанавливаем протокол, по которому отправлять (SMTP)
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
+=begin 
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'megakebabs.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp 
+=end
+
+=begin 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'megakebabs.heroku.com',
+    :enable_starttls_auto => true
+  }  
+=end
 end
