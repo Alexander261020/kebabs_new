977fac068a1d4c495999ba177828d51f9e59e4fb
alexandr <myltiplaz@gmail.com>
2022-01-20 13:08:12 +0500

    fixed error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
   
     if @new_comment.save
       # уведомляем всех подписчиков о новом комментарии
-      notify_subscribers(@event, @new_comment)
+      notify_subscribers(@new_comment)
       # Если сохранился, редирект на страницу самого события
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
@@ -49,16 +49,16 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:body, :user_name)
   end
 
-  def notify_subscribers(event, comment)
+  def notify_subscribers(comment)
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = (comment.event.subscriptions.map(&:user_email) + [comment.event.user.email]).uniq
     # Удаляем автора из рассылки
     all_emails.delete(comment.user.email) if comment.user&.email
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
     all_emails.each do |mail|
-      EventMailer.comment(event, comment, mail).deliver_now
+      EventMailer.comment(comment, mail).deliver_now
     end
   end
 end
